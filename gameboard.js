278b6bda0df5328c5ca97523f1736da014010417
Max Dendy <80255339+MaxKamenskii@users.noreply.github.com>
2026-01-10 13:12:32 +0500

    Implement receiveAttack function

--- gameboard.js
+++ gameboard.js
@@ -8,21 +8,21 @@ class Gameboard {
   }
   placeShip(ship, coordinates) {
     let unit = {};
-    ship = new Ship(coordinates.length);
+    // ship = new Ship(coordinates.length);
     coordinates.forEach((el) => {
       let key = el.toString();
       unit[key] = ship;
     });
     this.ships.push(unit);
   }
-  getShipAt(coordinate) {
+  getShipAt(coordinates) {
     // lets say it's [0,0]
     // Iterate over everyship on the board
     for (let ship of this.ships) {
       let found;
       // check if this ship has coordinate
-      if (ship.hasOwnProperty(coordinate.toString())) {
-        let key = coordinate.toString();
+      if (ship.hasOwnProperty(coordinates.toString())) {
+        let key = coordinates.toString();
         return ship[key];
       }
       // when finished searching check if found still false and return
@@ -30,6 +30,19 @@ class Gameboard {
         return "not found";
       }
     }
+  }
+  receiveAttack(coordinates) {
+    // Iterate over every ship on the board
+    for (let ship of this.ships) {
+      let found;
+      // check if this ship has the coordinates
+      if (ship.hasOwnProperty(coordinates.toString())) {
+        let key = coordinates.toString();
+        ship[key].hit();
+        return;
+      }
+    }
+    return "missed the shot";
   }
 }
 
--- gameboard.test.js
+++ gameboard.test.js
@@ -22,3 +22,27 @@ test("place ship and sarch at sea", () => {
   ]);
   expect(myGameboard.getShipAt([1, 0])).toBe("not found");
 });
+
+test("receive attack", () => {
+  const myGameboard = new Gameboard();
+  const ship = new Ship(3);
+  myGameboard.placeShip(ship, [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+  myGameboard.receiveAttack([0, 0]);
+  expect(ship.damage).toBe(1);
+});
+
+test("receive missed attack", () => {
+  const myGameboard = new Gameboard();
+  const ship = new Ship(3);
+  myGameboard.placeShip(ship, [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+  myGameboard.receiveAttack([1, 0]);
+  expect(ship.damage).toBe(0);
+});
